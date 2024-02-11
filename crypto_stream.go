b047875c3506ef246882a2aa91901299ad332051
johnbrandenburg <john.brandenburg@stord.com>
2024-02-10 20:48:14 -0500

    initial commit

---
+++ crypto_stream.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata"
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata/stream"
+)
+
+func CryptoStream() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Creating a client that connects to iex
+	c := stream.NewCryptoClient(marketdata.US,
+		stream.WithLogger(stream.DefaultLogger()),
+		// configuring initial subscriptions and handlers
+		stream.WithCryptoTrades(func(ct stream.CryptoTrade) {
+			fmt.Printf("TRADE: %+v\n", ct)
+		}, "*"),
+		stream.WithCryptoQuotes(func(cq stream.CryptoQuote) {
+			fmt.Printf("QUOTE: %+v\n", cq)
+		}, "BTC/USD"),
+		stream.WithCryptoOrderbooks(func(cob stream.CryptoOrderbook) {
+			fmt.Printf("ORDERBOOK: %+v\n", cob)
+		}, "BTC/USD"),
+	)
+	if err := c.Connect(ctx); err != nil {
+		panic(err)
+	}
+	if err := <-c.Terminated(); err != nil {
+		panic(err)
+	}
+}
---
+++ server.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	// "fmt"
+	"log"
+	"os"
+
+	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+
+	client := alpaca.NewClient(alpaca.ClientOpts{
+		APIKey:    os.Getenv("ALPACA_KEY"),
+		APISecret: os.Getenv("ALPACA_SECRET"),
+		BaseURL:   os.Getenv("ALPACA_URL"),
+	})
+
+	r.GET("/account", func(c *gin.Context) {
+		acct, err := client.GetAccount()
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, *acct)
+	})
+	r.Run(":8181") // listen and serve on 0.0.0.0:8080
+}
